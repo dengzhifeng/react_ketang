98159c1946d84f87d37c98b19bfb995e8e714566
邓志锋 <398577351@qq.com>
2021-10-28 08:11:13 +0800

    feat:2021-10-28-08:11:08

---
+++ src/routes/Detail/index.tsx
@@ -0,0 +1,9 @@
+/*
+ * @description:
+ * @author: steve.deng
+ * @Date: 2021-10-28 07:39:47
+ * @LastEditors: steve.deng
+ * @LastEditTime: 2021-10-28 08:11:00
+ */
+import React from 'react';
+function Detail(props: Props) {}
